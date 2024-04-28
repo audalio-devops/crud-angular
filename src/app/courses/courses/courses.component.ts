2787695a65f6fda38049efa76a5c63017237ac43
Audalio Devops <audalio.devops@gmail.com>
2024-04-28 01:39:38 -0300

    Customizacao de cor e modulo para importar Material, criacao de dataSource para exibir dados em tabela

--- src/app/courses/courses/courses.component.ts
+++ src/app/courses/courses/courses.component.ts
@@ -1,12 +1,30 @@
+import { AppMaterialModule } from '../../shared/app-material/app-material.module';
+import { Course } from './../model/course';
 import { Component } from '@angular/core';
+
 
 @Component({
   selector: 'app-courses',
   standalone: true,
-  imports: [],
+  imports: [AppMaterialModule],
   templateUrl: './courses.component.html',
   styleUrl: './courses.component.scss'
 })
+
 export class CoursesComponent {
+
+  courses: Course[] = [
+    { _id: "1", name: 'Angular', category:'front-end' },
+    { _id: "2", name: 'React', category:'front-end' },
+    { _id: "3", name: 'CSharp', category:'full stack' },
+    { _id: "4", name: 'Java', category:'back-end' },
+
+  ];
+  displayedColumns = ['name', 'category'];
+
+  //Também Pode inicializar dentro do constructor
+  //constructor() {
+  //  this.courses = [];
+  //}
 
 }

5385919d1d02c10fa04708d4c743c8fcac4eb226
audalio-devops <audalio.devops@gmail.com>
2024-05-04 00:53:39 -0300

    Implentacao usando NgForm funcionando corretamente

--- src/app/courses/course-form/course-form.component.ts
+++ src/app/courses/course-form/course-form.component.ts
@@ -1,35 +1,27 @@
 import { Component } from '@angular/core';
-import { FormBuilder, FormGroup } from '@angular/forms';
 import { AppMaterialModule } from '../../shared/app-material/app-material.module';
+import { NgForm, FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-course-form',
   standalone: true,
-  imports: [AppMaterialModule],
+  imports: [AppMaterialModule, FormsModule],
   templateUrl: './course-form.component.html',
   styleUrl: './course-form.component.scss'
 })
 export class CourseFormComponent {
 
+  constructor() { }
+
   onCancel() {
     console.log('onCancel :');
-    console.log(this.form.value);
   }
 
-  onSubmit() {
-    console.log('onSubmit :');
-    console.log(this.form.value);
-
-  }
-
-  form : FormGroup;
-
-  constructor (private formBuilder: FormBuilder) {
-    this.form = this.formBuilder.group({
-      name : [null],
-      category : [null]
-    });
-
+  onSubmit(form: NgForm): void {
+    if (form.valid) {
+      const formData = form.value;
+      console.log('Dados do formulário:', formData);
+    }
   }
 
 }
